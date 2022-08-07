0afebc090fb01404388e11021bdd2ccaf45b8313
ToxichQQQ <storozhenko.dev@gmail.com>
2022-08-08 01:48:23 +0300

    Fixed QuickSort

--- Quicksort.js
+++ Quicksort.js
@@ -1,43 +1,29 @@
-let items = [5,3,7,6,2,9];
+let items = [5,3,7,6,2,9,4,6,1,4,2,9,5,0,2,6,9,3,9,9,6,11,44,88,22,33,55,88,44,-5,2];
+let count = 0
 
-function swap(items, leftIndex, rightIndex){
-    let temp = items[leftIndex];
-    items[leftIndex] = items[rightIndex];
-    items[rightIndex] = temp;
-}
-function partition(items, left, right) {
-    let pivot   = items[Math.floor((right + left) / 2)], //middle element
-        i       = left, //left pointer
-        j       = right; //right pointer
-    while (i <= j) {
-        while (items[i] < pivot) {
-            i++;
-        }
-        while (items[j] > pivot) {
-            j--;
-        }
-        if (i <= j) {
-            swap(items, i, j); //sawpping two elements
-            i++;
-            j--;
+function quickSort(array){
+    if (array.length <= 1){
+        return array
+    }
+
+    let middleIndex = Math.floor(array.length / 2)
+    let middleItem = array[middleIndex]
+
+    let less = []
+    let great = []
+
+    for (let i = 0;i< array.length;i++){
+        count++
+        if(i === middleIndex) continue
+
+        if (array[i] < middleItem){
+            less.push(array[i])
+        }else{
+            great.push(array[i])
         }
     }
-    return i;
+    return [...quickSort(less),middleItem,...quickSort(great)]
 }
 
-function quickSort(items, left, right) {
-    let index;
-    if (items.length > 1) {
-        index = partition(items, left, right); //index returned from partition
-        if (left < index - 1) { //more elements on the left side of the pivot
-            quickSort(items, left, index - 1);
-        }
-        if (index < right) { //more elements on the right side of the pivot
-            quickSort(items, index, right);
-        }
-    }
-    return items;
-}
-// first call to quick sort
-let sortedArray = quickSort(items, 0, items.length - 1);
-console.log(sortedArray); //prints [2,3,5,6,7,9]
+console.log(quickSort(items))
+console.log(count)
